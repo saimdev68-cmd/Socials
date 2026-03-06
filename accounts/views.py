41e53989ba37c841e7102bf7caa222e755e09838
saimdev68-cmd <saim.dev68@gmail.com>
2026-03-06 15:15:07 +0500

    Social Logins

---
+++ accounts/views.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+
+# Create your views here.
+
+class HomeView(LoginRequiredMixin,generic.TemplateView):
+    template_name = "home.html"
